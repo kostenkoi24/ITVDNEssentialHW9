9447ac9a154675c957402a71c1abe04a3f430880
kostenkoi24 <kostenko.i@gmail.com>
2022-12-28 00:24:53 +0200

    Homework9

---
+++ Homework9_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Homework9_3
+{
+    class Program
+    {
+
+        delegate int MyDelegate();
+
+        static Random random = new Random();
+
+        static int m1() { int a = random.Next(1, 5); Console.WriteLine(a); return a;  }
+        static int m2() { int a = random.Next(1, 5); Console.WriteLine(a); return a; }
+        static int m3() { int a = random.Next(1, 5); Console.WriteLine(a); return a; }
+
+
+        static void Main(string[] args)
+        {
+
+
+            MyDelegate[] myDelegates = new MyDelegate[]
+            {
+                new MyDelegate(m1),
+                new MyDelegate(m2),
+                new MyDelegate(m3)
+            };
+
+            MyDelegate d = delegate
+            {
+                int sum = 0;
+                for (int i = 0; i < myDelegates.Length; i++)
+                {
+                    sum += myDelegates[i].Invoke();
+                }
+                return sum / myDelegates.Length;
+            };
+
+
+            Console.WriteLine("Avarage = {0}", d.Invoke());
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework9_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework9_5
+{
+
+    delegate int MyDelegate(int a, int b, int c);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            int c = 6;
+
+
+            MyDelegate d = delegate (int a, int b, int c) { return (a + b + c) / 3; };
+
+
+            int sum = d(a, b, c);
+
+            Console.WriteLine("Avarage of {0} {1} {2} = {3}", a, b, c, sum);
+
+            Console.ReadKey();
+        }
+    }
+}
